72c9245723490aa65396e86d31443540aa3fb543
Denis Nikonov <nikon1624@gmail.com>
2019-05-16 18:19:42 +0500

    section contacts

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,6 @@ var autoprefixer = require('autoprefixer');
 var webp = require('gulp-webp');
 var rename = require('gulp-rename');
 var svgstore = require('gulp-svgstore');
-var cheerio = require('gulp-cheerio');
-var cleanSvg = require('gulp-cheerio-clean-svg'); //npm install Hiswe/gulp-cheerio-clean-svg --save-dev
-var replace = require('gulp-replace');
-var concat = require('gulp-concat');
 
 gulp.task('sass', function () {
   return gulp.src('source/scss/**/style.scss')
@@ -44,26 +40,9 @@ gulp.task('webp', function () {
 });
 
 gulp.task('sprite', function() {
-  return gulp.src('source/img/**/fill-*.svg')
-  .pipe(cheerio(cleanSvg({
-    attributes: ["id", "style", "fill*", "clip*", "stroke*", "mask", "opacity", "width", "height", "transform"]
-  })))
-  .pipe(replace('&gt;', '>'))
+  return gulp.src('source/img/icons/**/*.svg')
   .pipe(svgstore({inlineSvg: true}))
   .pipe(rename('sprite.svg'))
-  .pipe(gulp.dest('source/img'))
-});
-
-gulp.task('sprite-attribute', function() {
-  return gulp.src(['!source/img/**/fill-*.svg', '!source/img/**/svg-sprite.svg', 'source/img/**/*.svg'])
-  .pipe(svgstore({inlineSvg: true}))
-  .pipe(rename('sprite-attribute.svg'))
-  .pipe(gulp.dest('source/img'))
-});
-
-gulp.task('concat-sprite', function() {
-  return gulp.src('source/img/**/sprite*.svg')
-  .pipe(concat('svg-sprite.svg'))
   .pipe(gulp.dest('source/img'))
 });
 
@@ -71,7 +50,7 @@ gulp.task('watch', function () {
   gulp.watch('source/scss/**/*.scss', gulp.parallel('sass'));
   gulp.watch('source/scss/**/*.scss', gulp.parallel('browser-sync'));
   gulp.watch(['source/img/**/*.png', 'source/img/**/*.jpg'], gulp.parallel('webp'));
-  gulp.watch('source/img/**/*.svg', gulp.parallel('sprite', 'sprite-attribute', 'concat-sprite'));
+  gulp.watch('source/img/icons/**/*.svg', gulp.parallel('sprite'));
 });
 
 gulp.task('default', gulp.parallel('watch'));
